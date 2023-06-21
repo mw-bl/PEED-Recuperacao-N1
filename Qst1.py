2865843606a5836bac819ae7dd23c730f697371d
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-06-21 19:57:06 -0300

    Questão 1

---
+++ Qst1.py
@@ -0,0 +1,8 @@
+print('Verificador de cupom') 
+  
+cupom = int(input('\nInsira o número do seu cupom: ')) 
+  
+if  75 <= cupom <= 208: 
+     print('Cupom válido!') 
+else: 
+     print('Cupom inválido.')
